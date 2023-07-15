d8e9d637f6eaf7c263027d9b979a696dee2f8842
Guilherme Steffens <99515689+steffensG@users.noreply.github.com>
2023-07-15 17:55:56 -0300

    Add files via upload

---
+++ SA Atualizado/tela comprar/script.js
@@ -0,0 +1,14 @@
+
+function MostrarLojas(){
+    document.getElementById('div-retirada').style.display = 'block'
+    document.getElementById('div-delivery').style.display = 'none'
+}
+
+function Endereco(){
+    document.getElementById('div-retirada').style.display = 'none'
+    document.getElementById('div-delivery').style.display = 'block'
+}
+
+// function Finalizar{
+    
+// }
